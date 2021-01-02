84ff29a8ef1df212d9b689c1bab1201e2d365a2f
Andrew Wood <andrewwood90@hotmail.com>
2021-01-02 11:40:31 +0000

    one test passed

---
+++ tests/player_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from app.models.player import *
+
+class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player_1 = Player('Robert', 'rock')
+        self.player_2 = Player('Philippa', 'paper')
+        self.player_3 = Player('Siobhan', 'scissors')
+
+
+    def test_player_has_name(self):
+        self.assertEqual('Robert', self.player_1.name)
